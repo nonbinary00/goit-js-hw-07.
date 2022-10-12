a0986c50596abd303883b72237d70fab0af26377
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-12 23:41:53 +0200

    srart
    start adding markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createImgMarkup ( galleryItems){
+    const markup = gallery.map(({preview, original, description})=>{
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="small-image.jpg"
+            data-source="large-image.jpg"
+            alt="Image description"/>
+        </a>
+      </div>`
+    }).join("");}
+
